7f74b110f5469be04658c628c89eaf127f234758
inviosolutions-rgb <inviosolutions@gmail.com>
2026-07-01 10:20:32 +0530

    Add error handler middleware to app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-
+import { errorHandler } from "./middleware/error.middleware";
 import routes from "./routes";
 
 const app = express();
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
-
+app.use(errorHandler);
 app.use("/api", routes);
 
 app.get("/", (_req, res) => {
